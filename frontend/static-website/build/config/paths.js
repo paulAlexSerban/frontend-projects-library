f4b75fc9efb5557e0f05ede2ca02a2645e7f0873
paulAlexSerban <paul.alex.serban@gmail.com>
2022-08-10 13:45:31 +0300

    refactor(wip): wip

--- frontend/static-website/build/config/paths.js
+++ frontend/static-website/build/config/paths.js
@@ -1,37 +1,21 @@
 export const paths = {
   src: {
-    assets: {
-      assetFiles: ["../../../assets/dist/**/*"],
-      svgFiles: ["dist/assets/svgs"]
-    },
     meta: {
       metaFiles: "./source/meta/*"
+    },
+    assets: {
+      assetFiles: ["../../assets/dist/**/*"],
+      svgFiles: ["dist/assets/svgs"]
     },
     html: {
       htmlDir: "./source",
       htmlFiles:"./source/html/*.html"
     },
     styles: {
-      scssDir: `../../living-style-guide/source/`,
-      scssFiles: [`../../living-style-guide/source/**/*.scss`],
-      scssPages: [`../../living-style-guide/source/**/pages/**/*.page.scss`],
-      scssLayers: [
-        `../../living-style-guide/source/modules/structure/pages/**/*.page.layer.scss`,
-        `../../living-style-guide/source/modules/structure/templates/**/*.template.layer.scss`,
-        `../../living-style-guide/source/modules/structure/grid/**/*.grid.layer.scss`
-      ],
-      scssComponents: [
-        `../../living-style-guide/source/**/components/**/*.atom.scss`,
-        `../../living-style-guide/source/**/components/**/*.molecule.scss`,
-        `../../living-style-guide/source/**/components/**/*.organism.scss`,
-        // `../../living-style-guide/source/**/templates/**/*.template.scss`,
-        // `../../living-style-guide/source/**/templates/**/*.page.scss`,
-      ],
-    },
-    scripts: {
-      javaScriptDir: `../../js-component-library/source`,
-      javaScriptFiles: [`../../js-component-library/source/**/*.js`],
-      javascriptPages: [`../../js-component-library/source/**/pages/**/*.page.js`]
+      scssDir: `../living-style-guide/source/`,
+      scssFiles: [`../living-style-guide/source/**/*.scss`],
+      scssPages: [`../living-style-guide/source/**/*.page.scss`, `../living-style-guide/source/**/*.page.devTools.scss`],
+      scssLayers: [`../living-style-guide/source/**/*.layer.scss`],
     }
   },
   dist: {
--- frontend/static-website/build/index.js
+++ frontend/static-website/build/index.js
@@ -3,43 +3,26 @@ import { paths } from "./config/paths";
 import { clean } from "./tasks/clean";
 
 import { lintHtml } from "./tasks/lintHtml";
-import { processHtml } from "./tasks/processHtml";
-import { processAssets } from "./tasks/processAssets";
+import { lintScss } from "./tasks/lintScss";
+
+import { compileHtml } from "./tasks/compileHtml";
+import { compileScss } from "./tasks/compileScss";
+
 import { processMetaFiles } from "./tasks/processMetaFiles";
-
-import { compileScssPages } from "./tasks/compileScssPages";
-// import { compileScssLayers } from "./tasks/compileScssLayers";
-// import { compileScssComponents } from "./tasks/compileScssComponents";
-
-import { transpileJavaScriptPages } from "./tasks/transpileJavaScriptPages";
-// import { minifyCss } from "./tasks/minifyCss";
+import { processAssets } from "./tasks/processAssets";
 
 // ---------------------------------------------------------------------
 // | Helper tasks                                                      |
 // ---------------------------------------------------------------------
 
-task("clean", clean);
-task("lint:html", lintHtml);
-task("lint", parallel("lint:html"));
-// task("test:scss", testScss);
-// task("test:scripts", testsScripts);
-// task("test", parallel("test:scss", "test:scripts"));
-task("process:assets", series(processAssets));
-task("process:html", series(lintHtml, processHtml, processMetaFiles));
-task("compile:styles", series(compileScssPages));
-task("transpile:javascript", series(transpileJavaScriptPages));
-// task("minify:styles", minifyCss);
-// task("minify:javaScript", minifyJavaScript)
-// task("minify", parallel("minify:styles", "minify:javaScript"));
-
 // ---------------------------------------------------------------------
 // | Main tasks                                                        |
 // ---------------------------------------------------------------------
 
-task("build", series("clean", "process:assets", parallel("process:html", "compile:styles", "transpile:javascript")));
+task("lint", parallel(lintHtml, lintScss));
+task("build", series(clean, processAssets, parallel(processMetaFiles, compileHtml, compileScss)));
 
 task("watch", () => {
-  watch(paths.src.html.htmlFiles, series("process:html"));
-  watch(paths.src.styles.scssFiles, series("compile:styles"));
-  watch(paths.src.scripts.javaScriptFiles, series("transpile:javascript"));
+  watch(paths.src.html.htmlFiles, series(lintHtml, compileHtml));
+  watch(paths.src.styles.scssFiles, series(lintScss, compileScss));
 });
